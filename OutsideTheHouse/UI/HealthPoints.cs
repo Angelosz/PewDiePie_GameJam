7ed6644f4eb0d4e90e357508ab9e47178f06f6bd
Angel Cabrera Gil <angelcabgil@gmail.com>
2014-11-22 19:01:41 +0000

    UI Shows healthPoints and the key

---
+++ OutsideTheHouse/UI/HealthPoints.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization.Formatters;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthPoints : MonoBehaviour
+{
+	public Sprite fullHeart;
+	public Sprite emptyHeart;
+	public GameObject player;
+	public int hp;
+
+	void Update()
+	{
+		this.GetComponent<Image>().sprite = (player.GetComponent<PlayerOutside>().healthPoints >= hp) ? fullHeart : emptyHeart;
+	}
+}
---
+++ OutsideTheHouse/UI/Key.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Key : MonoBehaviour
+{
+	public Sprite key;
+
+	public void changeKey()
+	{
+		this.GetComponent<Image>().sprite = key;
+	}
+}

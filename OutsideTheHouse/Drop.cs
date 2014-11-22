7a77ad76a6016461760c3e9c1c16e6a695bb332a
Angel Cabrera Gil <angelcabgil@gmail.com>
2014-11-22 16:54:40 +0000

    EnemyWithKey now drops the key

---
+++ OutsideTheHouse/Drop.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drop : MonoBehaviour
+{
+	public GameObject key;
+	public void DropKey()
+	{
+		Instantiate(key, this.transform.position, Quaternion.identity);
+	}
+}
--- OutsideTheHouse/Enemy.cs
+++ OutsideTheHouse/Enemy.cs
@@ -6,15 +6,24 @@ public class Enemy : MonoBehaviour
 	public GameObject player;
 	public bool patrolling = true;
 	public int speed;
-	public int healthpoints;
+	public int healthPoints;
 
 	public float getSpeed()
 	{
 		return speed * Time.deltaTime;
 	}
 
-	public void ReceiveDamge()
+	public void ReceiveDamage()
 	{
-		healthpoints--;
+		healthPoints--;
+		if (healthPoints <= 0)
+		{
+			if (this.GetComponent<Drop>() != null)
+				this.GetComponent<Drop>().DropKey();
+			Destroy(this.gameObject);
+		}
+			
 	}
+
+
 }

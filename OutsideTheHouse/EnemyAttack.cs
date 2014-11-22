d9523f2bcd5ab166f4657bf26cdc16b042ae6b49
Angel Cabrera Gil <angelcabgil@gmail.com>
2014-11-22 16:42:25 +0000

    EnemyScripts

---
+++ OutsideTheHouse/EnemyAttack.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour 
+{
+	private Enemy myEnemyScriptEnemy;
+	private float attackSpeed;
+	private float attackSpeedTimer;
+
+	void Start()
+	{
+		attackSpeed = 2f;
+		attackSpeedTimer = 0;
+		myEnemyScriptEnemy = this.GetComponent<Enemy>();
+	}
+
+	void Update()
+	{
+		if (!myEnemyScriptEnemy.patrolling)
+			if(CheckAttackSpeed())
+				CheckDistance();
+	}
+
+	private void CheckDistance()
+	{
+		if (Vector3.Distance(this.transform.position, myEnemyScriptEnemy.player.transform.position) < 1.6f)
+			AttackPlayer();
+	}
+
+	private void AttackPlayer()
+	{
+		myEnemyScriptEnemy.player.GetComponent<PlayerOutside>().ReceiveDamage();
+	}
+
+	private bool CheckAttackSpeed()
+	{
+		attackSpeedTimer += Time.deltaTime;
+		if (attackSpeedTimer > attackSpeed)
+		{
+			attackSpeedTimer = 0;
+			return true;
+		}
+		return false;
+	}
+}

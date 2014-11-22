de617102b788d55cbc456c9f2e5c5e92db89fe9f
Angel Cabrera Gil <angelcabgil@gmail.com>
2014-11-22 18:05:53 +0000

    Timer running folders a bit more organized

---
+++ OutsideTheHouse/UI/Timer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+	private float timer = 180;
+
+	void Update()
+	{
+		timer -= Time.deltaTime;
+		this.GetComponent<Text>().text = 
+			((((int) timer/60) > 9) ? ((int) timer/60).ToString() : "0" + ((int) timer/60)) + ":" +
+			((((int) timer%60) > 9) ? ((int) timer%60).ToString() : "0" + ((int) timer%60));
+	}
+}

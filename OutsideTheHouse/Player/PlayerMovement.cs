39b48e08b6207645fd3617018432802da6b742f4
Angel Cabrera Gil <angelcabgil@gmail.com>
2014-11-22 21:43:53 +0000

    PlayerMovementModified and player stats added

--- OutsideTheHouse/Player/PlayerMovement.cs
+++ OutsideTheHouse/Player/PlayerMovement.cs
@@ -3,18 +3,11 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-
 	private readonly int speed = 3;
 	
-	// Update is called once per frame
 	void Update () 
 	{
 		this.transform.Translate(new Vector3(Input.GetAxis("Horizontal") * 
 			Time.deltaTime * speed, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed));
-	}
-
-	void OnCollisionEnter(Collision collision)
-	{
-		Debug.Log("Choque!");
 	}
 }
---
+++ OutsideTheHouse/Player/PlayerOutside.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerOutside : MonoBehaviour 
+{
+	public int healthPoints;
+
+	void Start()
+	{
+		healthPoints = 3;
+	}
+
+	public void ReceiveDamage()
+	{
+		healthPoints--;
+		Debug.Log(healthPoints);
+	}
+}
